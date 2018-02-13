a0c0a94929edc86fc1d2f661eb77bc030193afc9
Lee Graham-Merrett <lmerrett@LEMERRETTMBP.local>
2018-02-13 08:58:52 +0000

    Added logo, added ability to change q and keep track of answers

---
+++ src/Question.jsx
@@ -0,0 +1,163 @@
+import React, { Component } from 'react';
+
+
+class Question extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            qOneAnswer: 0,
+            qTwoAnswer: 0,
+            qThreeAnswer: 0,
+            qFourAnswer: 0,
+            correctAnswers: 0,
+            currentQuestion: 1
+        };
+    }
+
+    updateAnswer1(update) {
+        this.setState({qOneAnswer: update.target.value});
+    }
+
+    updateAnswer2(update) {
+        this.setState({qTwoAnswer: update.target.value});
+    }
+
+    render() {
+        return (
+            <div>
+                <Question1
+                currentQuestion = {this.state.currentQuestion}
+                updateAnswer1 = {update => this.updateAnswer1(update)}
+                />
+                <Question2
+                currentQuestion = {this.state.currentQuestion}
+                updateAnswer2 = {update => this.updateAnswer2(update)}
+                />
+                <div className="button-row">
+                    <button className="next-q-button" type="button" onClick = {() => this.setState({currentQuestion: this.state.currentQuestion + 1})}>Next Question <i className="fa fa-chevron-right"></i></button>
+                </div>
+            </div>
+        );
+    }
+
+
+}
+
+function Question1(props) {
+    if (props.currentQuestion == 1){
+        return (
+            <div>
+                <h2 className="question-title">Question 1: THIS IS QUESTION ONE</h2>
+                    <div className="answer-block">
+                    <div className="answer-option">
+                        <label htmlFor="answer-one">Yes</label>
+                        <input
+                        id="answer-one"
+                        type="radio"
+                        name="answer"
+                        value="1"
+                        onChange={props.updateAnswer1}
+                        />
+                    </div>
+                    <div className="answer-option">
+                        <label htmlFor="answer-two">Maybe</label>
+                        <input
+                        id="answer-two"
+                        type="radio"
+                        name="answer"
+                        value="2"
+                        onChange={props.updateAnswer1}
+                        />
+                    </div>
+                    <div className="answer-option">
+                        <label htmlFor="answer-three">Yes3</label>
+                        <input
+                        id="answer-three"
+                        type="radio"
+                        name="answer"
+                        value="3"
+                        onChange={props.updateAnswer1}
+                        />
+                    </div>
+                    <div className="answer-option">
+                        <label htmlFor="answer-four">No!</label>
+                        <input
+                        id="answer-four"
+                        type="radio"
+                        name="answer"
+                        value="4"
+                        onChange={props.updateAnswer1}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    return null;
+}
+function Question2(props) {
+    if (props.currentQuestion == 2){
+        return (
+            <div>
+                <h2 className="question-title">Question 2: Test Q2 </h2>
+                <div className="answer-block">
+                    <div className="answer-option">
+                        <label htmlFor="answer-one">Yes</label>
+                        <input
+                            id="answer-one"
+                            type="radio"
+                            name="answer"
+                            value="1"
+                            onChange={props.updateAnswer2}
+                        />
+                    </div>
+                    <div className="answer-option">
+                        <label htmlFor="answer-two">Maybe</label>
+                        <input
+                            id="answer-two"
+                            type="radio"
+                            name="answer"
+                            value="2"
+                            onChange={props.updateAnswer2}
+                        />
+                    </div>
+                    <div className="answer-option">
+                        <label htmlFor="answer-three">Yes3</label>
+                        <input
+                            id="answer-three"
+                            type="radio"
+                            name="answer"
+                            value="3"
+                            onChange={props.updateAnswer2}
+                        />
+                    </div>
+                    <div className="answer-option">
+                        <label htmlFor="answer-four">No!</label>
+                        <input
+                            id="answer-four"
+                            type="radio"
+                            name="answer"
+                            value="4"
+                            onChange={props.updateAnswer2}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    return null;
+}
+
+export default Question;
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,59 +1,15 @@
 import React, { Component } from 'react';
+import Question from './Question.jsx';
 
 class Quiz extends Component {
     constructor(props) {
-        super(props)
-    }
-    this.state = {
-        currentAnswer: 0,
-        correctAnswers: 0
+        super(props);
     }
 
     render() {
         return (
-            <div class="question-block">
-                <h2 class="question-title">Question 1: I saw a girl today who had 12 nipples. Sounds mad, dozen tit?</h2>
-                <div class="answer-block">
-                    <div class="answer-option">
-                        <label for="answer-one">Yes</label>
-                        <input
-                            id="answer-one"
-                            type="radio"
-                            name="answer"
-                            value="answer-one"
-                        />
-                    </div>
-                    <div class="answer-option">
-                        <label for="answer-two">Yes2</label>
-                        <input
-                            id="answer-two"
-                            type="radio"
-                            name="answer"
-                            value="answer-two"
-
-                        />
-                    </div>
-                    <div class="answer-option">
-                        <label for="answer-three">Yes3</label>
-                        <input
-                            id="answer-three"
-                            type="radio"
-                            name="answer"
-                            value="answer-three"
-
-                        />
-                    </div>
-                    <div class="answer-option">
-                        <label for="answer-four">No!</label>
-                        <input
-                            id="answer-four"
-                            type="radio"
-                            name="answer"
-                            value="answer-four"
-
-                        />
-                    </div>
-                </div>
+            <div className="question-block">
+                <Question />
             </div>
         );
     }
