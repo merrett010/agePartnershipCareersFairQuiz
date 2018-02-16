ec629c4dd1d998b19b2d2604fda8a582eea231a6
Lee Graham-Merrett <lmerrett@LEMERRETTMBP.local>
2018-02-16 14:53:11 +0000

    added landing page and name & email field

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -10,7 +10,9 @@ class Questions extends Component {
             qThreeAnswer: 0,
             qFourAnswer: 0,
             correctAnswers: 0,
-            currentQuestion: 1
+            currentQuestion: 0,
+            userEmail: 'default@test.com',
+            userName: ''
         };
     }
 
@@ -22,17 +24,32 @@ class Questions extends Component {
         this.setState({qTwoAnswer: update.target.value});
     }
 
+    updateEmail(update) {
+        this.setState({userEmail: update.target.value});
+    }
+
+    updateName(update) {
+        this.setState({userName: update.target.value});
+    }
+
     render() {
         return (
             <div>
-                <Question1
-                    currentQuestion = {this.state.currentQuestion}
-                    updateAnswer1 = {update => this.updateAnswer1(update)}
-                />
-                <Question2
-                    currentQuestion = {this.state.currentQuestion}
-                    updateAnswer2 = {update => this.updateAnswer2(update)}
-                />
+                { this.state.currentQuestion === 0
+                    ? <Landing
+                        userEmail = {this.state.userEmail}
+                        updateEmail = {email => this.updateEmail(email)}
+                        />
+                    : null
+                }
+                { this.state.currentQuestion === 1
+                    ? <Question1 updateAnswer1 = {update => this.updateAnswer1(update)} />
+                    : null
+                }
+                { this.state.currentQuestion === 2
+                    ? <Question2 updateAnswer2 = {update => this.updateAnswer2(update)} />
+                    : null
+                }
                 <div className="button-row">
                     <button className="next-q-button" type="button" onClick = {() => this.setState({currentQuestion: this.state.currentQuestion + 1})}>Next Question <i className="fa fa-chevron-right"></i></button>
                 </div>
@@ -41,110 +58,122 @@ class Questions extends Component {
     }
 }
 
-function Question1(props) {
-    if (props.currentQuestion == 1){
-        return (
-            <div>
-                <h2 className="question-title">Question 1: THIS IS QUESTION ONE</h2>
-                    <div className="answer-block">
-                    <div className="answer-option">
-                        <label htmlFor="answer-one">Yes</label>
-                        <input
-                            id="answer-one"
-                            type="radio"
-                            name="answer"
-                            value="1"
-                            onChange={props.updateAnswer1}
-                        />
-                    </div>
-                    <div className="answer-option">
-                        <label htmlFor="answer-two">Maybe</label>
-                        <input
-                            id="answer-two"
-                            type="radio"
-                            name="answer"
-                            value="2"
-                            onChange={props.updateAnswer1}
-                        />
-                    </div>
-                    <div className="answer-option">
-                        <label htmlFor="answer-three">Yes3</label>
-                        <input
-                            id="answer-three"
-                            type="radio"
-                            name="answer"
-                            value="3"
-                            onChange={props.updateAnswer1}
-                        />
-                    </div>
-                    <div className="answer-option">
-                        <label htmlFor="answer-four">No!</label>
-                        <input
-                            id="answer-four"
-                            type="radio"
-                            name="answer"
-                            value="4"
-                            onChange={props.updateAnswer1}
-                        />
-                    </div>
+function Landing(props) {
+    return (
+        <div>
+            <h2 className="central-block gutter-bottom-30">Please fill out the form to get started! </h2>
+            <div className="central-block">
+                <div className="gutter-bottom-10">
+                    <label className="font16" htmlFor="emailInput">Name: </label>
+                    <input className="font16" id="emailInput" type="text" onChange={props.updateName} value={props.userName}/>
+                </div>
+                <div className="gutter-bottom-10">
+                    <label className="font16" htmlFor="emailInput">Email: </label>
+                    <input className="font16" id="emailInput" type="email" onChange={props.updateEmail} value={props.userEmail}/>
                 </div>
             </div>
-        );
-    }
-    return null;
+        </div>
+    );
+}
+
+function Question1(props) {
+    return (
+        <div>
+            <h2 className="question-title">Question 1: THIS IS QUESTION ONE</h2>
+                <div className="answer-block">
+                <div className="answer-option">
+                    <label htmlFor="answer-one">Yes</label>
+                    <input
+                        id="answer-one"
+                        type="radio"
+                        name="answer"
+                        value="1"
+                        onChange={props.updateAnswer1}
+                    />
+                </div>
+                <div className="answer-option">
+                    <label htmlFor="answer-two">Maybe</label>
+                    <input
+                        id="answer-two"
+                        type="radio"
+                        name="answer"
+                        value="2"
+                        onChange={props.updateAnswer1}
+                    />
+                </div>
+                <div className="answer-option">
+                    <label htmlFor="answer-three">Yes3</label>
+                    <input
+                        id="answer-three"
+                        type="radio"
+                        name="answer"
+                        value="3"
+                        onChange={props.updateAnswer1}
+                    />
+                </div>
+                <div className="answer-option">
+                    <label htmlFor="answer-four">No!</label>
+                    <input
+                        id="answer-four"
+                        type="radio"
+                        name="answer"
+                        value="4"
+                        onChange={props.updateAnswer1}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 function Question2(props) {
-    if (props.currentQuestion == 2){
-        return (
-            <div>
-                <h2 className="question-title">Question 2: Test Q2 </h2>
-                <div className="answer-block">
-                    <div className="answer-option">
-                        <label htmlFor="answer-one">Yes</label>
-                        <input
-                            id="answer-one"
-                            type="radio"
-                            name="answer"
-                            value="1"
-                            onChange={props.updateAnswer2}
-                        />
-                    </div>
-                    <div className="answer-option">
-                        <label htmlFor="answer-two">Maybe</label>
-                        <input
-                            id="answer-two"
-                            type="radio"
-                            name="answer"
-                            value="2"
-                            onChange={props.updateAnswer2}
-                        />
-                    </div>
-                    <div className="answer-option">
-                        <label htmlFor="answer-three">Yes3</label>
-                        <input
-                            id="answer-three"
-                            type="radio"
-                            name="answer"
-                            value="3"
-                            onChange={props.updateAnswer2}
-                        />
-                    </div>
-                    <div className="answer-option">
-                        <label htmlFor="answer-four">No!</label>
-                        <input
-                            id="answer-four"
-                            type="radio"
-                            name="answer"
-                            value="4"
-                            onChange={props.updateAnswer2}
-                        />
-                    </div>
+    return (
+        <div>
+            <h2 className="question-title">Question 2: Test Q2 </h2>
+            <div className="answer-block">
+                <div className="answer-option">
+                    <label htmlFor="answer-one">Yes</label>
+                    <input
+                        id="answer-one"
+                        type="radio"
+                        name="answer"
+                        value="1"
+                        onChange={props.updateAnswer2}
+                    />
+                </div>
+                <div className="answer-option">
+                    <label htmlFor="answer-two">Maybe</label>
+                    <input
+                        id="answer-two"
+                        type="radio"
+                        name="answer"
+                        value="2"
+                        onChange={props.updateAnswer2}
+                    />
+                </div>
+                <div className="answer-option">
+                    <label htmlFor="answer-three">Yes3</label>
+                    <input
+                        id="answer-three"
+                        type="radio"
+                        name="answer"
+                        value="3"
+                        onChange={props.updateAnswer2}
+                    />
+                </div>
+                <div className="answer-option">
+                    <label htmlFor="answer-four">No!</label>
+                    <input
+                        id="answer-four"
+                        type="radio"
+                        name="answer"
+                        value="4"
+                        onChange={props.updateAnswer2}
+                    />
                 </div>
             </div>
-        );
-    }
-    return null;
+        </div>
+    );
 }
 
 export default Questions;
