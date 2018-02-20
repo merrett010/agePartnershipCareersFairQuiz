529a260fa72e26c4cd03398102385e232a5f2a35
Lee Graham-Merrett <lee.graham-merrett@themarketinglab.co.uk>
2018-02-20 21:41:44 +0000

    Made it prettier and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1 className="banner-text">You shall not pass... this quiz!</h1>
+                <h1 className="banner-text bold">You shall not pass... this quiz!</h1>
                 <Quiz />
             </div>
         );
